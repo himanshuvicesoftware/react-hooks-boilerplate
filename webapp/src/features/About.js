6032b3eba8d2997b74e8c1758eb030b5b46e16f2
AakashThapa <aakash.thapa@vicesoftware.com>
2020-04-20 11:45:28 +0530

    Integrated Pdftron and customized the header for the needed tools.

--- webapp/src/features/About.js
+++ webapp/src/features/About.js
@@ -1,95 +1,92 @@
-import React, { Component } from 'react'
-import { Document, Page, pdfjs } from 'react-pdf'
-import 'react-pdf/dist/Page/AnnotationLayer.css'
-import samplePDF from './pdfForm.pdf'
+import React, { useEffect, useRef } from 'react'
+import WebViewer from '@pdftron/webviewer'
+import './about.styles.css'
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+const Test = () => {
+	const viewer = useRef(null)
 
-export default class Test extends Component {
-	state = {
-		numPages: null,
-		pageNumber: 1,
-		scale: 1,
-	}
+	useEffect(() => {
+		WebViewer(
+			{
+				path: 'webViewer',
+				initialDoc: 'sampleFile.pdf',
+				disabledElements: [
+					'stickyToolButton',
+					'miscToolGroupButton',
+					'menuButton',
+					'searchButton',
+					'panToolButton',
+					'viewControlsButton',
+					'viewControlsOverlay',
+					'selectToolButton',
+					'signatureToolButton',
+					'shapeToolGroupButton',
+					// 'textToolGroupButton',
+					'leftPanelButton',
+				],
+			},
+			viewer.current
+		).then((instance) => {
+			const {
+				docViewer,
+				Annotations,
+				FitMode,
+				setFitMode,
+				LayoutMode,
+				setLayoutMode,
+				Feature,
+				disableFeatures,
+			} = instance
 
-	onDocumentLoadSuccess = (document) => {
-		const { numPages } = document
-		this.setState({
-			numPages,
-			pageNumber: 1,
+			instance.setHeaderItems((header) => {
+				header.push(
+					{
+						type: 'actionButton',
+						img:
+							'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
+						onClick: () => {
+							docViewer.setCurrentPage(docViewer.getCurrentPage() + 1)
+						},
+					},
+					{
+						type: 'actionButton',
+						img:
+							'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
+						onClick: () => {
+							docViewer.setCurrentPage(docViewer.getCurrentPage() - 1)
+						},
+					}
+				)
+			})
+
+			setLayoutMode(LayoutMode.Single)
+			setFitMode(FitMode.Zoom)
+			disableFeatures([Feature.PageNavigation])
+
+			const annotManager = docViewer.getAnnotationManager()
+
+			docViewer.on('documentLoaded', () => {
+				const rectangleAnnot = new Annotations.RectangleAnnotation()
+				rectangleAnnot.PageNumber = 1
+				// values are in page coordinates with (0, 0) in the top left
+				rectangleAnnot.X = 100
+				rectangleAnnot.Y = 150
+				rectangleAnnot.Width = 200
+				rectangleAnnot.Height = 50
+				rectangleAnnot.Author = annotManager.getCurrentUser()
+				annotManager.addAnnotation(rectangleAnnot)
+				// need to draw the annotation otherwise it won't show up until the page is refreshed
+				annotManager.redrawAnnotation(rectangleAnnot)
+			})
 		})
-	}
+	}, [])
 
-	zoomIn = () => {
-		this.setState({ scale: this.state.scale + 0.5 })
-	}
-
-	zoomOut = () => {
-		this.setState({ scale: this.state.scale - 0.5 })
-	}
-
-	changePage = (offset) =>
-		this.setState((prevState) => ({
-			pageNumber: prevState.pageNumber + offset,
-		}))
-
-	previousPage = () => this.changePage(-1)
-
-	nextPage = () => this.changePage(1)
-
-	render() {
-		const { numPages, pageNumber } = this.state
-
-		return (
-			<React.Fragment>
-				<div>
-					{/* <p>
-						Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
-					</p> */}
-					<button
-						type='button'
-						disabled={this.state.scale === 2}
-						onClick={this.zoomIn}
-					>
-						ZoomIn
-					</button>
-					<button
-						type='button'
-						disabled={this.state.scale === 1}
-						onClick={this.zoomOut}
-					>
-						ZoomOut
-					</button>
-
-					<button
-						type='button'
-						disabled={pageNumber <= 1}
-						onClick={this.previousPage}
-					>
-						Previous
-					</button>
-					<button
-						type='button'
-						disabled={pageNumber >= numPages}
-						onClick={this.nextPage}
-					>
-						Next
-					</button>
-				</div>
-				<Document
-					file={samplePDF}
-					externalLinkTarget='_blank'
-					onLoadSuccess={this.onDocumentLoadSuccess}
-				>
-					<Page
-						renderAnnotationLayer={true}
-						renderInteractiveForms={true}
-						scale={this.state.scale}
-						pageNumber={pageNumber}
-						renderTextLayer={false}
-					/>
-				</Document>
-			</React.Fragment>
-		)
-	}
+	return (
+		<div className='MyComponent'>
+			<div className='header'>React sample</div>
+			<div className='webviewer' ref={viewer}></div>
+		</div>
+	)
 }
+
+export default Test
