481f107528abc0e8f9b9fa320103f9897aeb7039
himanshuvicesoftware <himanshu.pant@vicesoftware.com>
2020-04-22 21:08:09 +0530

    Updated NavTab

--- webapp/src/widgets/NavBar/NavBar.js
+++ webapp/src/widgets/NavBar/NavBar.js
@@ -2,50 +2,94 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import Dropdown from 'react-bootstrap/Dropdown'
 import { LinkContainer } from 'react-router-bootstrap'
 import { selectIsAuthenticated, logout } from '../../features/userContext'
+import './NavBar.css'
+const bellIcon = require('../../assets/icons/Bell.svg')
 
 export default function NavBar() {
 	const isAuthenticated = useSelector(selectIsAuthenticated)
 	const dispatch = useDispatch()
 	return (
-		<Navbar bg='dark' variant='dark'>
-			<LinkContainer to='/'>
-				<Navbar.Brand>Vice Software</Navbar.Brand>
-			</LinkContainer>
-			<Nav>
-				<LinkContainer to='/users'>
-					<Nav.Link>Users</Nav.Link>
-				</LinkContainer>
-			</Nav>
-			<Nav>
-				<LinkContainer to='/authenticated'>
-					<Nav.Link>Authenticated</Nav.Link>
-				</LinkContainer>
-			</Nav>
-			<Nav>
-				<LinkContainer to='/authorized'>
-					<Nav.Link>Authorized</Nav.Link>
-				</LinkContainer>
-			</Nav>
-			<Nav>
-				<LinkContainer to='/settings'>
-					<Nav.Link>Settings</Nav.Link>
-				</LinkContainer>
-			</Nav>
-			<Navbar.Collapse className='justify-content-end'>
-				{isAuthenticated ? (
-					<Navbar.Text>
-						<a href='#sign-in' onClick={() => dispatch(logout())}>
-							Logout
-						</a>
-					</Navbar.Text>
-				) : (
-					<LinkContainer to='/sign-in'>
-						<Nav.Link>Sign In</Nav.Link>
-					</LinkContainer>
-				)}
-			</Navbar.Collapse>
+		<Navbar bg='white'>
+			<div className='container-fluid pl-0'>
+				<Navbar.Collapse className='collapse px-4 px-lg-0'>
+					<Nav className='mr-auto align-items-lg-center '>
+						<Nav.Item className='pl-lg-4 ml-lg-2 active'>
+							<LinkContainer to='/'>
+								<Nav.Link>Dashboard</Nav.Link>
+							</LinkContainer>
+						</Nav.Item>
+						<Nav.Item className=' pl-lg-4 ml-lg-2'>
+							<LinkContainer to='/'>
+								<Nav.Link>Documents</Nav.Link>
+							</LinkContainer>
+						</Nav.Item>
+						<Nav.Item className=' pl-lg-4 ml-lg-2'>
+							<button className='btn btn-primary font-weight-normal' href='#'>
+								Create Document
+							</button>
+						</Nav.Item>
+					</Nav>
+
+					<Nav className='nav-bar-right'>
+						<Nav.Item className='dropdown notification-dropdown mr-lg-1'>
+							<Dropdown className='nav-link position-relative'>
+								<Dropdown.Toggle variant='light'>
+									<span className='position-relative'>
+										<img
+											src={bellIcon}
+											alt=''
+											className='bell-icon'
+											width='25'
+										/>
+										<span className='badge badge-pill bg-primary text-white font-weight-normal '>
+											1
+										</span>
+									</span>
+								</Dropdown.Toggle>
+
+								<Dropdown.Menu className='dropdown-menu-right p-15 text-center shadow'>
+									<Dropdown.Item href='#'>
+										Cody Miles Created a New Version of #1235
+									</Dropdown.Item>
+								</Dropdown.Menu>
+							</Dropdown>
+						</Nav.Item>
+						<Nav.Item className='dropdown user-dropdown pl-lg-4 ml-lg-2'>
+							<Dropdown className='nav-link user-link'>
+								<Dropdown.Toggle variant='light' id='dropdown-basic'>
+									<span className='user-name f-12 bg-primary text-white rounded-circle font-weight-normal'>
+										CM
+									</span>
+								</Dropdown.Toggle>
+
+								<Dropdown.Menu className='dropdown-menu-right p-15 text-center shadow'>
+									<Dropdown.Item>
+										<LinkContainer to='/profile'>
+											<Nav.Link>My Profile</Nav.Link>
+										</LinkContainer>
+									</Dropdown.Item>
+									<Dropdown.Item>
+										{isAuthenticated ? (
+											<Navbar.Text>
+												<a href='#sign-in' onClick={() => dispatch(logout())}>
+													Logout
+												</a>
+											</Navbar.Text>
+										) : (
+											<LinkContainer to='/sign-in'>
+												<Nav.Link>Sign In</Nav.Link>
+											</LinkContainer>
+										)}
+									</Dropdown.Item>
+								</Dropdown.Menu>
+							</Dropdown>
+						</Nav.Item>
+					</Nav>
+				</Navbar.Collapse>
+			</div>
 		</Navbar>
 	)
 }
