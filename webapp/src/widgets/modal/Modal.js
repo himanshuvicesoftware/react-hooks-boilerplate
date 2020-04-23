f5de144b0ea4888f4de8db19b2930bb4473f5d4c
himanshuvicesoftware <himanshu.pant@vicesoftware.com>
2020-04-23 20:29:05 +0530

    #2250-Updated Modal infrastructure

--- webapp/src/widgets/modal/Modal.js
+++ webapp/src/widgets/modal/Modal.js
@@ -1,18 +1,32 @@
 import React from 'react'
-import Modal from 'react-bootstrap/Modal'
-import { useSelector } from 'react-redux'
+import { Modal, Image, Button } from 'react-bootstrap'
+import { useSelector, useDispatch } from 'react-redux'
 import { selectShowModal } from './modal.selectors'
 import { actions } from './modal.slice'
+import './Modal.css'
+const closeIcon = require('../../assets/icons/Close.svg')
 
-const { useHideModal } = actions
+const { hideModal } = actions
 
-export default function ViceModal({ children }) {
+export default function ViceModal({ children, header, footer }) {
 	const show = useSelector(selectShowModal)
-	const hideModal = useHideModal()
-
+	const dispatch = useDispatch()
 	return (
-		<Modal show={show} onHide={hideModal}>
-			{children}
+		<Modal show={show} onHide={() => dispatch(hideModal())}>
+			<Modal.Header>
+				<Modal.Title className='f-32 font-semi-bold'>{header}</Modal.Title>
+				<Button
+					type='button'
+					className='close p-0 pr-2'
+					onClick={() => dispatch(hideModal())}
+				>
+					<Image src={closeIcon} alt='' width='25' />
+				</Button>
+			</Modal.Header>
+			<Modal.Body>{children}</Modal.Body>
+			<Modal.Footer className='justify-content-start border-0'>
+				{footer}
+			</Modal.Footer>
 		</Modal>
 	)
 }
