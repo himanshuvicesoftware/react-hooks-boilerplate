70c354e3a2f2588e96fb7c89b22e62dc815b9386
AakashThapa <aakash.thapa@vicesoftware.com>
2020-04-28 16:54:38 +0530

    changes left out during rebase.

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import createStore from './createStore'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
