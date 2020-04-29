70ed4a841be4133e8ab86b6ef8825b4d0030aa02
himanshuvicesoftware <himanshu.pant@vicesoftware.com>
2020-04-29 14:46:40 +0530

    Re-Added Files

---
+++ webapp/src/features/DocumentArchive/DocumentArchiveSearch.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import { Card, Form, Button } from 'react-bootstrap'
+
+const DocumentArchiveSearch = () => {
+	const [searchByName, setSearchByName] = useState('')
+	const radioButtonTitle = [
+		'By Customer Name',
+		'By Creation Date',
+		'By Serial Number',
+		'By TSN',
+	]
+	return (
+		<Card className='shadow-sm'>
+			<Card.Body>
+				<Card.Title>Search</Card.Title>
+				<Form.Group>
+					<Form.Label>Filter by Document Status</Form.Label>
+					{radioButtonTitle.map((title, index) => (
+						<Form.Check
+							key={index}
+							type='radio'
+							label={title}
+							id={`document-Archive-Search-${index}`}
+							name='document-Archive-Search'
+						/>
+					))}
+				</Form.Group>
+				<Form.Group>
+					<Form.Control
+						type='text'
+						value={searchByName}
+						placeholder='Customer Name'
+						onChange={(ev) => setSearchByName(ev.target.value)}
+					/>
+				</Form.Group>
+				<Button className='btn-small' type='button' variant='secondary'>
+					Search
+				</Button>
+			</Card.Body>
+		</Card>
+	)
+}
+
+export default DocumentArchiveSearch
---
+++ webapp/src/features/DocumentArchive/DocumentArchiveTable.js
@@ -0,0 +1,145 @@
+import React from 'react'
+import { Form, Button } from 'react-bootstrap'
+import Table from '../../widgets/Table'
+
+const customCheckbox = (column) => (
+	<div className='custom-checkbox mr-2 d-inline-block'>
+		<Form.Check
+			type='checkbox'
+			label={typeof column === 'string' ? column : column.text}
+			id={`document-Archive-Search-${
+				typeof column === 'string' ? column : column.text
+			}`}
+		/>
+	</div>
+)
+
+const viewButton = () => (
+	<Button
+		variant='primary'
+		id='viewButton'
+		size='sm'
+		className='font-weight-normal'
+	>
+		View
+	</Button>
+)
+
+const mockDocData = [
+	{
+		customer: 'test1',
+		creation: 'Creation',
+		serialNumber: 12346,
+		status: 'online',
+		tsn: 79797,
+		assignedUser: 'New User',
+	},
+	{
+		customer: 'test2',
+		creation: 'Potter',
+		serialNumber: 12346,
+		status: 'offline',
+		tsn: 654498,
+		assignedUser: 'User',
+	},
+	{
+		customer: 'test3',
+		creation: 'Creation',
+		serialNumber: 12346,
+		status: 'online',
+		tsn: 79797,
+		assignedUser: 'New User',
+	},
+	{
+		customer: 'test4',
+		creation: 'Potter',
+		serialNumber: 12346,
+		status: 'offline',
+		tsn: 654498,
+		assignedUser: 'User',
+	},
+	{
+		customer: 'test5',
+		creation: 'Creation',
+		serialNumber: 12346,
+		status: 'online',
+		tsn: 79797,
+		assignedUser: 'New User',
+	},
+	{
+		customer: 'test6',
+		creation: 'Potter',
+		serialNumber: 12346,
+		status: 'offline',
+		tsn: 654498,
+		assignedUser: 'User',
+	},
+	{
+		customer: 'test7',
+		creation: 'Creation',
+		serialNumber: 12346,
+		status: 'online',
+		tsn: 79797,
+		assignedUser: 'New User',
+	},
+	{
+		customer: 'test8',
+		creation: 'Potter',
+		serialNumber: 12346,
+		status: 'offline',
+		tsn: 654498,
+		assignedUser: 'User',
+	},
+]
+const columns = [
+	{
+		dataField: 'customer',
+		text: 'Customer',
+		sort: true,
+		headerFormatter: customCheckbox,
+		formatter: customCheckbox,
+	},
+	{
+		dataField: 'creation',
+		text: 'Creation',
+		sort: true,
+	},
+	{
+		dataField: 'serialNumber',
+		text: 'Serial Number',
+		sort: true,
+	},
+	{
+		dataField: 'status',
+		text: 'Status',
+		sort: true,
+	},
+	{
+		dataField: 'tsn',
+		text: 'TSN',
+		sort: true,
+	},
+	{
+		dataField: 'assignedUser',
+		text: 'Assigned User',
+		sort: true,
+	},
+	{
+		dataField: 'viewButton',
+		text: '',
+		formatter: viewButton,
+	},
+]
+
+const DocumentArchiveTable = () => {
+	return (
+		<Table
+			keyField='customer'
+			columns={columns}
+			data={mockDocData}
+			rowClasses='text-secondry'
+		/>
+	)
+}
+
+export default DocumentArchiveTable
